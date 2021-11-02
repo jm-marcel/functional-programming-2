f9786b0995a1d36a74a2a50479f95ca40fa608ce
jm-marcel <joaomarcel040800@gmail.com>
2021-11-02 19:59:05 -0300

    feat: exercicio 3

--- fp.js
+++ fp.js
@@ -1,42 +1,42 @@
 const R = require("ramda");
 
-function isEven(number) {
+const isEven = (number) => {
   const n = R.clone(number);
   n.even = n.value % 2 == 0;
   return n;
-}
+};
 
-function isOdd(number) {
+const isOdd = (number) => {
   const n = R.clone(number);
   n.odd = n.value % 2 != 0;
   return n;
-}
+};
 
-function positive(number) {
+const positive = (number) => {
   const n = R.clone(number);
   n.positive = n.value > 0;
   return n;
-}
+};
 
-function negative(number) {
+const negative = (number) => {
   const n = R.clone(number);
   n.negative = n.value < 0;
   return n;
-}
+};
 
-function isZero(number) {
+const isZero = (number) => {
   const n = R.clone(number);
   n.zero = n.value == 0;
   return n;
-}
+};
 
-function isPrime(number) {
+const isPrime = (number) => {
   for (let i = 2; i < number; i++) return number % i === 0 ? false : number > 1;
-}
+};
 
-function mapToNumberObject(number) {
+const mapToNumberObject = (number) => {
   return { value: number };
-}
+};
 
 const arr = [-1, 50, 5, 10, -8, 20, 25, 0, 100, 14, -123];
 
